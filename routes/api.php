42f80ca5ece0153e669b8558869ebd7258d6037f
longfei <1371986272@qq.com>
2022-11-05 05:44:48 +0000

    route users rename register

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('v1')->name('api.v1.')->group(function (){
-    Route::post('/users', [\App\Http\Controllers\Api\UserController::class, 'store']);
+    Route::post('/register', [\App\Http\Controllers\Api\UserController::class, 'store']);
 
     Route::group([
         'prefix' => 'auth'
