fb3ee8f7565a66d7ae9b0fd83da851d5c1661c46
longfei <1371986272@qq.com>
2022-11-07 09:41:09 +0000

    add networdAvatar

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\Api\UserRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Http;
 
 class UserController extends Controller
 {
@@ -15,6 +16,8 @@ class UserController extends Controller
         $name = Str::random(6);
         $email = $request->email;
         $password = $request->password;
+
+        // 本地头像
         $path = '/images/avatar/default/';
         $avatars = [
             'avatar00.jpeg',
@@ -24,9 +27,21 @@ class UserController extends Controller
             'avatar04.jpeg',
         ];
         $avatar = $avatars[mt_rand(0, 4)];
+        $localAvatar = $path.$avatar;
+
+        // 网络头像
+        $response = Http::get('http://api.btstu.cn/sjtx/api.php?lx=c1&format=json&method=mobile');
+        if ( !empty($response->body()) ){
+            $json = json_decode($response->body());
+            if ( $json->code == 200 ){
+                $avatarUrl = $json->imgurl;
+            }else{
+                $avatarUrl = "";
+            }
+        }
 
         $user = User::create([
-            'avatar' => $path.$avatar,
+            'avatar' => $avatarUrl ?? $localAvatar,
             'name'  => $name,
             'email' => $email,
             'password' => bcrypt($password),
